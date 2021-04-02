1632d6e0f1ea77d59c7f88e7a097acf308e26a3d
tanhongit <phuongtan12357nguyen@gmail.com>
2021-04-02 17:25:17 +0700

    setup db

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +12,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('users')->insert([
+                'name' => "user{$i}",
+                'email' => "user{$i}@gmail.com",
+                'phone' => bcrypt('User@123'),
+            ]);
+        }
+        for ($i = 0; $i < 20; $i++) {
+            DB::table('students')->insert([
+                'name' => "user{$i}",
+                'email' => "user{$i}@gmail.com",
+                'phone' => "0123456789",
+            ]);
+        }
     }
 }
