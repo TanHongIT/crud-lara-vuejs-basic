25e11bcb7299fdab2c04f3cbcd00bb6979064b8a
tanhongit <phuongtan12357nguyen@gmail.com>
2021-04-02 09:10:34 +0700

    api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('save_student', 'StudentController@save_student');
+Route::get('all_students', 'StudentController@all_students');
+Route::get('edit_student/{id}', 'StudentController@edit_student');
+Route::put('update_student', 'StudentController@update_student');
+Route::delete('delete_student/{id}', 'StudentController@delete_student');
